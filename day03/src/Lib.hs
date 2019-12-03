7df3a62e9c742227c2c12fa78ac035b199a3becf
3v0k4 <riccardo.odone@gmail.com>
2019-12-03 20:48:55 +0100

    refactor day03

--- day03/src/Lib.hs
+++ day03/src/Lib.hs
@@ -7,93 +7,44 @@ module Lib
 
 import Data.List
 import Data.List.Split
-import Data.Maybe (fromJust)
+import Linear.V2
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 firstStar :: IO ()
 firstStar = do
-  xs <- fmap (splitOn ",") . lines <$> readFile "src/input"
-  case xs of (x:y:_) -> print $ closestM x y
+  xs <- lines <$> readFile "src/input"
+  let [ys, zs] = fmap (visited . parse) xs
+  print $ closestManhattan ys zs
+
+visited :: [V2 Int] -> Set (V2 Int)
+visited = Set.fromList . scanl1 (+)
+
+closestManhattan :: Set (V2 Int) -> Set (V2 Int) -> Int
+closestManhattan xs ys = minimum . Set.map manhattan $ xs `Set.intersection` ys
+
+manhattan :: V2 Int -> Int
+manhattan (V2 x y) = abs x + abs y
+
+parse :: String -> [V2 Int]
+parse = concatMap coord . splitOn ","
+  where
+    coord (x:xs) = replicate (read xs) $ case x of
+      'R' -> V2   1    0
+      'L' -> V2 (-1)   0
+      'U' -> V2   0    1
+      'D' -> V2   0  (-1)
 
 secondStar :: IO ()
 secondStar = do
-  xs <- fmap (splitOn ",") . lines <$> readFile "src/input"
-  case xs of (x:y:_) -> print $ closestS x y
+  xs <- lines <$> readFile "src/input"
+  let [ys, zs] = fmap (visited2 . parse) xs
+  print $ closestSteps ys zs
 
-origin = (0,0)
+visited2 :: [V2 Int] -> Map (V2 Int) Int
+visited2 = Map.fromListWith min . flip zip [1..] . scanl1 (+)
 
-type Coord = (Int,Int)
-type Line = (Coord,Coord)
-
-linez :: [String] -> [Line]
-linez xs = go xs origin []
-  where
-    go []     _ acc = reverse acc
-    go (z:zs) (x,y) acc = go zs new (line : acc)
-      where
-        line = ((x,y), new)
-
-        new =
-          case read <$> splitAt 1 z of
-            ("R",t) -> (x+t,y)
-            ("L",t) -> (x-t,y)
-            ("U",t) -> (x,y+t)
-            ("D",t) -> (x,y-t)
-
-overlap :: Line -> Line -> [Coord]
-overlap ((xa,ya),(xb,yb)) ((wa,za),(wb,zb)) =
-  if parallel then
-    []
-  else
-    [ (x,y)
-    | x <- [min xa xb..max xa xb] `intersect` [min wa wb..max wa wb]
-    | y <- [min ya yb..max ya yb] `intersect` [min za zb..max za zb]
-    ]
-  where
-    parallel = (xa == xb && wa == wb && xa /= wa) || (ya == yb && za == zb && ya /= za)
-
-overlaps :: [Line] -> [Line] -> [Coord]
-overlaps xs ys = concat $
-  [ overlap l1 l2
-  | l1 <- xs
-  , l2 <- ys
-  ]
-
-closestM :: [String] -> [String] -> Int
-closestM w1 w2 =
-  minimum ds
-  where
-    xs = overlaps (linez w1) (linez w2)
-    ys = filter (/= (0,0)) xs
-    ds = manh origin <$> ys
-
-manh :: (Int,Int) -> (Int,Int) -> Int
-manh (x,y) (x',y') = abs (x-x') + abs (y-y')
-
-closestS :: [String] -> [String] -> Int
-closestS w1 w2 =
-  minimum ds
-  where
-    xs = overlaps (linez w1) (linez w2)
-    ys = filter (/= (0,0)) xs
-    ds = (\c -> steps w1 c + steps w2 c) <$> ys
-
-steps :: [String] -> Coord -> Int
-steps xs c@(x,y) = fst $ fromJust $ find ((==) c . snd) $ zip [0..] $ coords (flatten xs)
-
-flatten :: [String] -> [String]
-flatten xs = expand `concatMap` xs
-  where
-    expand s = replicate (read t) d
-      where
-        (d,t) = splitAt 1 s
-
-coords :: [String] -> [(Int,Int)]
-coords xs = go [origin] origin xs
-  where
-    go acc _     []     = reverse acc
-    go acc (x,y) (z:zs) =
-      case z of
-        "R" -> let c = (x+1,y) in go (c:acc) c zs
-        "L" -> let c = (x-1,y) in go (c:acc) c zs
-        "U" -> let c = (x,y+1) in go (c:acc) c zs
-        "D" -> let c = (x,y-1) in go (c:acc) c zs
+closestSteps :: Map (V2 Int) Int -> Map (V2 Int) Int -> Int
+closestSteps xs ys = minimum $ Map.intersectionWith (+) xs ys
